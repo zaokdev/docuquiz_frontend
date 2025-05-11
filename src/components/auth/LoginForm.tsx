245a6d4bf3fd5cd0e300a06ef3fa2bd8997cac2f
zaokdev-laptop <kevinzaok@outlook.com>
2025-05-10 18:33:37 -0600

    Agregado parte de la lógica de login y register

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,57 +1,90 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 /* eslint-disable react/jsx-sort-props */
 /* eslint-disable prettier/prettier */
-import React from 'react'
-import {Form} from "@heroui/form"
-import { Input } from '@heroui/input'
-import { Button } from '@heroui/button'
-
+import React, { useState } from "react";
+import { Form } from "@heroui/form";
+import { Input } from "@heroui/input";
+import { Button } from "@heroui/button";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
+  const [user, setUser] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState<any>();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:3000/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: user, password }),
+      credentials: "include",
+    });
+    console.log(response);
+
+    if (!response.ok) {
+      console.log(response);
+      setError(response.statusText);
+      console.log(await response.json());
+      return;
+    }
+
+    navigate({
+      pathname: "/",
+    });
+  };
   return (
-    <main className='container flex mx-auto items-center justify-center border max-h-auto'>
-    <div className='grid grid-cols-12 p-12'>
+    <main className="container flex flex-col mx-auto items-center justify-center border max-h-auto">
+      <div className="grid grid-cols-12 p-12">
         <Form
-      className="w-full max-w-xs flex flex-col gap-4 col-span-6"
-      onSubmit={(e:any) => {
-        e.preventDefault();
+          className="w-full max-w-xs flex flex-col gap-4 col-span-12 lg:col-span-6"
+          onSubmit={handleSubmit}
+        >
+          <Input
+            isRequired
+            errorMessage="Please enter a valid username"
+            label="Username"
+            labelPlacement="outside"
+            name="username"
+            placeholder="Enter your username"
+            type="text"
+            size="lg"
+            onValueChange={(e: any) => {
+              setUser(e);
+            }}
+          />
 
-      }}
-    >
-      <Input
-        isRequired
-        errorMessage="Please enter a valid username"
-        label="Username"
-        labelPlacement="outside"
-        name="username"
-        placeholder="Enter your username"
-        type="text"
-        size='lg'
-      />
-
-      <Input
-        isRequired
-        errorMessage="Please enter password"
-        label="Password"
-        labelPlacement="outside"
-        name="password"
-        placeholder="Enter your password"
-        type="password"
-        size='lg'
-      />
-      <div className="flex gap-2">
-        <Button color="primary" type="submit" size='lg'>
-          Iniciar Sesión
-        </Button>
+          <Input
+            isRequired
+            errorMessage="Please enter password"
+            label="Password"
+            labelPlacement="outside"
+            name="password"
+            placeholder="Enter your password"
+            type="password"
+            size="lg"
+            onValueChange={(e: any) => {
+              setPassword(e);
+            }}
+          />
+          <div className="flex gap-2">
+            <Button color="primary" type="submit" size="lg">
+              Iniciar Sesión
+            </Button>
+          </div>
+        </Form>
+        <img
+          alt="HeroUI hero Image"
+          src="https://heroui.com/images/hero-card-complete.jpeg"
+          className="col-span-6 rounded-r-3xl lg:block hidden"
+        />
       </div>
-    </Form>
-    <img       
-    alt="HeroUI hero Image"
-      src="https://heroui.com/images/hero-card-complete.jpeg"
-      className='col-span-6 rounded-r-3xl'/>
-    </div>
+      {error && <span>{error}</span>}
     </main>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
--- src/components/auth/RegisterForm.tsx
+++ src/components/auth/RegisterForm.tsx
@@ -1,64 +1,110 @@
 /* eslint-disable prettier/prettier */
-import React from 'react'
-import {Form} from "@heroui/form"
-import { Input } from '@heroui/input'
-import { Button } from '@heroui/button'
+import React, { useState } from "react";
+import { Form } from "@heroui/form";
+import { Input } from "@heroui/input";
+import { Button } from "@heroui/button";
+import { useNavigate } from "react-router-dom";
 
 const RegisterForm = () => {
+  const [user, setUser] = useState();
+  const [password, setPassword] = useState();
+  const [email, setEmail] = useState();
+  const [error, setError] = useState<any>();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const formData = {
+      username: user,
+      password,
+      email,
+    };
+    const response = await fetch("http://localhost:3000/api/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      setError(response.statusText);
+      return;
+    }
+
+    if (data.message) {
+      setError(data.response.message);
+      return;
+    }
+
+    navigate({
+      pathname: "/auth/login",
+    });
+  };
   return (
-    <main className='container flex mx-auto items-center justify-center border max-h-auto'>
-    <div className='grid grid-cols-12 p-12'>
+    <main className="container flex flex-col mx-auto items-center justify-center max-h-auto">
+      <div className="grid grid-cols-12 p-12 border">
         <Form
-      className="w-full max-w-xs flex flex-col gap-4 col-span-6"
-      onSubmit={(e:any) => {
-        e.preventDefault();
+          className="w-full max-w-xs flex flex-col gap-4 col-span-12 lg:col-span-6"
+          onSubmit={handleSubmit}
+        >
+          <Input
+            isRequired
+            errorMessage="Please enter a valid username"
+            label="Username"
+            labelPlacement="outside"
+            name="username"
+            placeholder="Enter your username"
+            type="text"
+            size="lg"
+            onValueChange={(e: any) => {
+              setUser(e);
+            }}
+          />
+          <Input
+            isRequired
+            errorMessage="Please enter email"
+            label="Email"
+            labelPlacement="outside"
+            name="email"
+            placeholder="Enter your email"
+            type="email"
+            size="lg"
+            onValueChange={(e: any) => {
+              setEmail(e);
+            }}
+          />
+          <Input
+            isRequired
+            errorMessage="Please enter password"
+            label="Password"
+            labelPlacement="outside"
+            name="password"
+            placeholder="Enter your password"
+            type="password"
+            size="lg"
+            onValueChange={(e: any) => {
+              setPassword(e);
+            }}
+          />
 
-      }}
-    >
-      <Input
-        isRequired
-        errorMessage="Please enter a valid username"
-        label="Username"
-        labelPlacement="outside"
-        name="username"
-        placeholder="Enter your username"
-        type="text"
-        size='lg'
-      />
-        <Input
-        isRequired
-        errorMessage="Please enter email"
-        label="Email"
-        labelPlacement="outside"
-        name="email"
-        placeholder="Enter your email"
-        type="email"
-        size='lg'
-      />
-      <Input
-        isRequired
-        errorMessage="Please enter password"
-        label="Password"
-        labelPlacement="outside"
-        name="password"
-        placeholder="Enter your password"
-        type="password"
-        size='lg'
-      />
-
-      <div className="flex gap-2">
-        <Button color="primary" type="submit" size='lg'>
-          Registrar
-        </Button>
+          <div className="flex gap-2">
+            <Button color="primary" type="submit" size="lg">
+              Registrar
+            </Button>
+          </div>
+        </Form>
+        <img
+          alt="HeroUI hero Image"
+          src="https://heroui.com/images/hero-card-complete.jpeg"
+          className="col-span-6 rounded-r-3xl lg:block hidden"
+        />
       </div>
-    </Form>
-    <img       
-    alt="HeroUI hero Image"
-      src="https://heroui.com/images/hero-card-complete.jpeg"
-      className='col-span-6 rounded-r-3xl'/>
-    </div>
+      {error && <span>{error}</span>}
     </main>
-  )
-}
+  );
+};
 
-export default RegisterForm
+export default RegisterForm;
